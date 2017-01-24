7ef4b4c89bd2b73e106d07795e1b7d238a8c9d8e
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-24 17:09:07 +0000

    not particularly interested what Item spec does until I have inventory class

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -3,7 +3,7 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     items = [ new Item("foo", 0, 0) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
   });
 
 });
