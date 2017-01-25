d60bb89ecbcbee441cf099268f3e9ead3eb36057
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-25 09:59:57 +0000

    remove old method, and add setter methods for Item

--- spec/itemSpec.js
+++ spec/itemSpec.js
@@ -1,9 +1,37 @@
-describe("Gilded Rose", function() {
+describe("Item", function() {
+  var item
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("foo");
+  beforeEach(function(){
+    item = new Item("An Item", 7, 7)
+  });
+
+  describe("#increaseQuality", function() {
+
+    it("should increase the item's quality", function() {
+      item.increaseQuality(3);
+      expect(item.quality).toEqual(10);
+    });
+
+  });
+
+  describe("#descreaseQuality", function() {
+
+    it("should decrease the item's quality", function() {
+      item.decreaseQuality(2);
+      expect(item.quality).toEqual(5);
+    });
+
+  });
+
+  describe("#decreaseSellIn", function() {
+
+    it("should decrease the sellby date of an item", function() {
+      item.decreaseSellIn(1);
+      expect(item.sell_in).toEqual(6);
+    });
+
   });
 
 });
+
+// functions for setting and getting name, quality, sell_in
--- src/item.js
+++ src/item.js
@@ -1,57 +1,21 @@
 // The Item class and its properties must not be changed unless you want to be
 // one-shotted by the goblin in the corner
+//
+// The Item class holds data about an item
 function Item(name, sell_in, quality) {
   this.name = name;
   this.sell_in = sell_in;
   this.quality = quality;
-}
+};
 
-var items = []
+Item.prototype.increaseQuality = function(number) {
+  this.quality += number;
+};
 
-function update_quality() {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
-    }
-  }
-}
+Item.prototype.decreaseQuality = function(number) {
+  this.quality -= number;
+};
+
+Item.prototype.decreaseSellIn = function(number) {
+  this.sell_in -= number
+};
