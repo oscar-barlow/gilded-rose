512d1738a2205325dd238cf6d2135a79b9a3e103
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-25 15:36:15 +0000

    add tons of doubles to itemupdater spec

--- spec/itemUpdaterSpec.js
+++ spec/itemUpdaterSpec.js
@@ -3,22 +3,37 @@ describe("ItemUpdater", function() {
   var freshItem;
   var staleItem;
   var legendaryItem;
+  var agedItem;
+  var conjuredItem;
+  var backstagePass;
 
   beforeEach(function() {
-    freshItem = jasmine.createSpyObj('freshItem', ['name','decreaseSellIn', 'decreaseQuality', 'isPastSellIn', 'isLegendary']);
-    staleItem = jasmine.createSpyObj('staleItem', ['name','decreaseSellIn', 'decreaseQuality', 'isPastSellIn', 'isLegendary']);
-    legendaryItem = jasmine.createSpyObj('legendaryItem', ['name','decreaseSellIn', 'decreaseQuality', 'isPastSellIn', 'isLegendary']);
+    methods = ['name','decreaseSellIn', 'decreaseQuality', 'increaseQuality', 'isPastSellIn', 'isLegendary', 'isAged', 'isConjured', 'isBackStagePass']
+    freshItem = jasmine.createSpyObj('freshItem', methods);
+    staleItem = jasmine.createSpyObj('staleItem', methods);
+    legendaryItem = jasmine.createSpyObj('legendaryItem', methods);
+    agedItem = jasmine.createSpyObj('agedItem', methods);
+    conjuredItem = jasmine.createSpyObj('Conjured Item', methods);
+    backstagePass = jasmine.createSpyObj('Backstage Pass', methods);
 
     legendaryItem.name.and.returnValue("A Legendary item");
     legendaryItem.isLegendary.and.returnValue(true);
 
     freshItem.name.and.returnValue("A Fresh item");
-    freshItem.isLegendary.and.returnValue(false);
 
     staleItem.name.and.returnValue("A Stale item");
-    staleItem.isLegendary.and.returnValue(false);
+    staleItem.isPastSellIn.and.returnValue(true);
 
-    updater = new ItemUpdater([freshItem, staleItem, legendaryItem]);
+    agedItem.name.and.returnValue("An Aged item");
+    agedItem.isAged.and.returnValue(true);
+
+    conjuredItem.name.and.returnValue("A Conjured Item");
+    conjuredItem.isConjured.and.returnValue(true);
+
+    backstagePass.name.and.returnValue("Backstage pass");
+    backstagePass.isBackStagePass.and.returnValue(true);
+
+    updater = new ItemUpdater([freshItem, staleItem, legendaryItem, agedItem, conjuredItem, backstagePass]);
   });
 
   describe("#initialize", function() {
@@ -61,8 +76,6 @@ describe("ItemUpdater", function() {
     });
 
     it("should tell items past their sell_in to decrease quality by 2", function() {
-      freshItem.isPastSellIn.and.returnValue(false);
-      staleItem.isPastSellIn.and.returnValue(true);
       updater.updateQuality();
       expect(staleItem.decreaseQuality).toHaveBeenCalledWith(2);
     });
@@ -70,6 +83,11 @@ describe("ItemUpdater", function() {
     it("should not decrease quality of legendary items", function() {
       updater.updateQuality();
       expect(legendaryItem.decreaseQuality).not.toHaveBeenCalled();
+    });
+
+    it("should increase the quality of aged items", function() {
+      updater.updateQuality();
+      expect(agedItem.increaseQuality).toHaveBeenCalledWith(1);
     });
 
   });
